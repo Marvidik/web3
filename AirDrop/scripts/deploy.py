d5b8dfc06b3a6c54612da35d167209d2ba213d57
unknown <ebubeidika@gmail.com>
2024-02-23 23:15:27 +0100

    Added something

--- AirDrop/scripts/deploy.py
+++ AirDrop/scripts/deploy.py
@@ -3,8 +3,8 @@ from brownie import Airdrop,accounts,network
 
 def deploy_bet():
     
-    recipient_address = "0x5f67AC1cE46699026B8E07C8418F26b148d6f9B1"
-    deployed_contract = Airdrop.deploy(recipient_address, {'from': accounts[2]})
+    recipient_address = accounts[1]
+    deployed_contract = Airdrop.deploy(recipient_address, {'from': accounts[0]})
 
     # Print contract address
     print("Contract deployed at:", deployed_contract.address)
--- AirDrop/scripts/functions.py
+++ AirDrop/scripts/functions.py
@@ -3,17 +3,17 @@ from brownie import Airdrop,accounts,network
 def displayAmount():
     betting=Airdrop[-1]
 
-    amount=betting.displayAmount({"from":"0x5f67AC1cE46699026B8E07C8418F26b148d6f9B1","value":50})
+    amount=betting.displayAmount({"from":accounts[1],"value":50})
 
 
 
 def transfer():
     trans=Airdrop[-1]
 
-    trans.transferEther({"from":"0x5f67AC1cE46699026B8E07C8418F26b148d6f9B1"})
+    trans.transferEther({"from":accounts[1]})
 
    
 
 def main():
-    transfer()
+    displayAmount()
     
